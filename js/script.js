d594f912fea2a33a26497364a92345311520e1c5
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-02-12 05:58:19 +1300

    apply header background when scrolled down

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@
 import './default.js';
 
 // ========== DOM references ==========
-
+const header = document.querySelector('header');
 
 // ========== global variables ==========
 
 
 // ========== script ==========
+// Detect scroll and apply background to the header(or navbar)
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 10) {
+        header.classList.add('header--active');
+    } else {
+        header.classList.remove('header--active');
+    };
+});
+
