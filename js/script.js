94e98ef7578e59c994a80740fc6086c0e68069dd
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-02-18 10:47:28 +1300

    parallax bg img

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const header = document.querySelector('header');
 const menuBtn = document.querySelector('.menu-btn');
 const menuIcn = document.querySelector('.menu-icn');
 const nav = document.querySelector('.nav');
+const aboutWho = document.querySelector('.about-who');
+const whoImg = document.querySelector('.who-img');
 
 // ========== global variables ==========
 
@@ -30,5 +32,18 @@ menuBtn.addEventListener('click', (e) => {
         menuIcn.classList.remove('fa-times');
         menuIcn.classList.add('fa-bars');
         nav.classList.remove('nav--active');
+    };
+});
+
+// Fixed bg - about-who
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > aboutWho.offsetTop - window.innerHeight && window.pageYOffset < aboutWho.offsetTop + aboutWho.scrollHeight - header.scrollHeight) {
+        const scroll = window.pageYOffset;
+        const startPosition = aboutWho.offsetTop - window.innerHeight;
+        const endPosition = aboutWho.offsetTop + aboutWho.scrollHeight - header.scrollHeight;
+        const scrollRatio = Math.round((scroll - startPosition) / (endPosition - startPosition) * 100);
+        const reverseScrollRatio = 100 - scrollRatio;
+        
+        whoImg.style.objectPosition = `50% ${reverseScrollRatio}%`;
     };
 });
