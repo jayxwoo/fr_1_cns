34130ebcf1e7c1b2d7bef47ff79a659222eb9a45
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-02-18 11:53:09 +1300

    load imgs using js

--- js/script.js
+++ js/script.js
@@ -8,44 +8,65 @@ const menuIcn = document.querySelector('.menu-icn');
 const nav = document.querySelector('.nav');
 const aboutWho = document.querySelector('.about-who');
 const whoImg = document.querySelector('.who-img');
+const partnersContainer = document.querySelector('.partners-container');
 
 // ========== global variables ==========
 
 
 // ========== script ==========
-// Detect scroll and apply background to the header(or navbar)
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 10) {
-        header.classList.add('header--active');
-    } else {
-        header.classList.remove('header--active');
-    };
-});
+const main = function () {
+    // Detect scroll and apply background to the header(or navbar)
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 10) {
+            header.classList.add('header--active');
+        } else {
+            header.classList.remove('header--active');
+        };
+    });
 
-// Menu button
-menuBtn.addEventListener('click', (e) => {
-    if (menuIcn.classList.contains('fa-bars')) {
-        menuIcn.classList.remove('fa-bars');
-        menuIcn.classList.add('fa-times');
-        nav.classList.add('nav--active');
-    } else if (menuIcn.classList.contains('fa-times')) {
-        menuIcn.classList.remove('fa-times');
-        menuIcn.classList.add('fa-bars');
-        nav.classList.remove('nav--active');
-    };
-});
+    // Menu button
+    menuBtn.addEventListener('click', (e) => {
+        if (menuIcn.classList.contains('fa-bars')) {
+            menuIcn.classList.remove('fa-bars');
+            menuIcn.classList.add('fa-times');
+            nav.classList.add('nav--active');
+        } else if (menuIcn.classList.contains('fa-times')) {
+            menuIcn.classList.remove('fa-times');
+            menuIcn.classList.add('fa-bars');
+            nav.classList.remove('nav--active');
+        };
+    });
 
-// Parallax background img (about-who section)
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > aboutWho.offsetTop - window.innerHeight && window.pageYOffset < aboutWho.offsetTop + aboutWho.scrollHeight - header.scrollHeight) {
-        const scroll = window.pageYOffset;
-        const startPosition = aboutWho.offsetTop - window.innerHeight;
-        const endPosition = aboutWho.offsetTop + aboutWho.scrollHeight - header.scrollHeight;
-        const scrollRatio = Math.round((scroll - startPosition) / (endPosition - startPosition) * 100);
-        const reverseScrollRatio = 100 - scrollRatio;
-        
-        whoImg.style.objectPosition = `50% ${reverseScrollRatio}%`;
-    };
-});
+    // Parallax background img (about-who section)
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > aboutWho.offsetTop - window.innerHeight && window.pageYOffset < aboutWho.offsetTop + aboutWho.scrollHeight - header.scrollHeight) {
+            const scroll = window.pageYOffset;
+            const startPosition = aboutWho.offsetTop - window.innerHeight;
+            const endPosition = aboutWho.offsetTop + aboutWho.scrollHeight - header.scrollHeight;
+            const scrollRatio = Math.round((scroll - startPosition) / (endPosition - startPosition) * 100);
+            const reverseScrollRatio = 100 - scrollRatio;
 
-// Load partner logo imgs
+            whoImg.style.objectPosition = `50% ${reverseScrollRatio}%`;
+        };
+    });
+
+    // Load partner logo imgs
+    const partnerLogoImgs = [];
+    const numOfPartnerLogoImgs = 10;
+
+    for (let i = 0; i < numOfPartnerLogoImgs; i++) {
+        const img = new Image();
+        img.src = `./img/partners/partner-${i + 1}.png`;
+        img.classList.add('partners-img');
+        img.setAttribute('alt', 'partner logo image');
+        partnerLogoImgs.push(img);
+    };
+
+    partnerLogoImgs.forEach(partnerLogoImg => {
+        const partnersItem = document.createElement('div');
+        partnersItem.appendChild(partnerLogoImg);
+        partnersItem.classList.add('partners-item');
+        partnersContainer.appendChild(partnersItem);
+    });
+};
+main();
