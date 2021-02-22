b025896537707f4b885afe58a967005f1565b0ae
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-02-23 08:30:23 +1300

    email validaiton regex test

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const header = document.querySelector('header');
 const menuBtn = document.querySelector('.menu-btn');
 const menuIcn = document.querySelector('.menu-icn');
 const nav = document.querySelector('.nav');
+const heroCompany = document.querySelector('.hero-company');
 const aboutWho = document.querySelector('.about-who');
 const whoImg = document.querySelector('.who-img');
 const partnersContainer = document.querySelector('.partners-container');
@@ -14,6 +15,8 @@ const animation_1 = document.querySelector('.animation-1');
 const animation_1_container = document.querySelector('.animation-1-container');
 const animation_1_img = document.querySelector('.animation-1-img');
 const animation_1_text = document.querySelector('.animation-1-text');
+const contactInputs = document.querySelectorAll('.contact-input');
+const emailInputFeedback = document.querySelector('.email-input-feedback');
 
 // ========== global variables ==========
 
@@ -43,6 +46,17 @@ const main = function () {
         };
     });
 
+    // Hero-company text flashing effect
+    setInterval(() => {
+        if (getComputedStyle(heroCompany).opacity === '1') {
+            heroCompany.style.opacity = '0.5';
+            heroCompany.style.transform = 'translateY(10%)';
+        } else {
+            heroCompany.style.opacity = '1';
+            heroCompany.style.transform = 'translateY(0)';
+        };
+    }, 1500);
+
     // Parallax background img (about-who section)
     window.addEventListener('scroll', () => {
         if (window.pageYOffset > aboutWho.offsetTop - window.innerHeight && window.pageYOffset < aboutWho.offsetTop + aboutWho.scrollHeight) {
@@ -54,27 +68,6 @@ const main = function () {
             whoImg.style.objectPosition = `50% ${100 - scrollRatio}%`;
         };
     });
-
-    // // Load partner logo imgs
-    // const partnerLogoImgs = [];
-    // const numOfPartnerLogoImgs = 9;
-
-    // for (let i = 0; i < numOfPartnerLogoImgs; i++) {
-    //     const img = new Image();
-    //     img.src = `./img/partners/partner-${i + 1}.png`;
-    //     img.classList.add('partners-img');
-    //     img.setAttribute('alt', 'partner logo image');
-    //     partnerLogoImgs.push(img);
-    // };
-
-    // partnerLogoImgs.forEach(partnerLogoImg => {
-    //     const partnersItem = document.createElement('div');
-    //     const partnersLink = document.createElement('a');
-    //     partnersItem.appendChild(partnersLink);
-    //     partnersItem.classList.add('partners-item');
-    //     partnersLink.appendChild(partnerLogoImg);
-    //     partnersContainer.appendChild(partnersItem);
-    // });
 
     // Load gallery imgs
     const galleryImgs = [];
@@ -95,50 +88,12 @@ const main = function () {
         galleryContainer.appendChild(galleryItem);
     });
 
-    // // animation-1
-    // window.addEventListener('scroll', () => {
-    //     // section 1 - fixed bg
-    //     if (window.pageYOffset > animation_1.offsetTop - window.innerHeight && window.pageYOffset < animation_1.offsetTop) {
-    //         animation_1_container.style.position = 'absolute';
-    //         animation_1_container.style.transform = 'scale(1)';
-    //         animation_1_text.style.transform = 'scale(1)';
-
-    //         const scroll = window.pageYOffset;
-    //         const startPosition = animation_1.offsetTop - window.innerHeight;
-    //         const endPosition = animation_1.offsetTop;
-    //         const scrollRatio = (scroll - startPosition) / (endPosition - startPosition);
-    //         animation_1_container.style.top = `${-window.innerHeight + window.innerHeight * scrollRatio}px`;
-    //     };
-
-    //     // section 2 - scale down
-    //     if (window.pageYOffset > animation_1.offsetTop && window.pageYOffset < animation_1.offsetTop + window.innerHeight) {
-    //         animation_1_container.style.position = 'fixed';
-    //         animation_1_container.style.top = 0;
-
-    //         const scroll = window.pageYOffset;
-    //         const startPosition = animation_1.offsetTop;
-    //         const endPosition = animation_1.offsetTop + window.innerHeight;
-    //         const scrollRatio = (scroll - startPosition) / (endPosition - startPosition);
-    //         animation_1_container.style.position = 'fixed';
-    //         animation_1_container.style.transform = `scale(${1 - (0.5 * scrollRatio)})`;
-
-    //         animation_1_text.style.transform = `scale(${1 + (50 * scrollRatio)})`;
-    //         // animation_1_text.style.fontSize = `${2 + (10 * scrollRatio)}rem`;
-    //     };
-
-    //     // section 3 - back to normal
-    //     if (window.pageYOffset > animation_1.offsetTop + window.innerHeight) {
-    //         animation_1_container.style.position = 'absolute';
-    //         animation_1_container.style.top = `${window.innerHeight}px`;
-    //     };
-    // });
-
     // animation-1
     window.addEventListener('scroll', () => {
         // section 0 - before animation
         if (window.pageYOffset < animation_1.offsetTop - window.innerHeight) {
             animation_1_img.style.display = 'none';
-        }
+        };
 
         // section 1 - fixed bg
         if (window.pageYOffset > animation_1.offsetTop - window.innerHeight && window.pageYOffset < animation_1.offsetTop) {
@@ -188,6 +143,40 @@ const main = function () {
             animation_1_container.style.top = `${window.innerHeight}px`;
             animation_1_text.style.display = 'none';
         };
+    });
+
+    // Regex test for email input
+    const pattern = {
+        email : /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+    };
+
+    const validate = function (value, pattern) {
+        let result = pattern.test(value);
+
+        if (result) {
+            emailInputFeedback.style.display = 'block';
+            emailInputFeedback.textContent = 'valid email';
+            emailInputFeedback.style.backgroundColor = 'var(--green)';
+
+            setTimeout(() => {
+                emailInputFeedback.style.display = 'none';
+            }, 1000);
+        } else {
+            emailInputFeedback.style.display = 'block';
+            emailInputFeedback.textContent = 'invalid email';
+            emailInputFeedback.style.backgroundColor = 'var(--red)';
+        };
+    };
+
+    contactInputs.forEach(input => {
+        input.addEventListener('keyup', (e) => {
+            if (e.target.value.length > 5) {
+                validate(e.target.value, pattern[e.target.name]);
+            };
+            if (e.target.value.length === 0) {
+                emailInputFeedback.style.display = 'none';
+            };
+        });
     });
 };
 main();
